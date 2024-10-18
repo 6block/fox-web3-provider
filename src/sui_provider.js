4b6b3fecd2a0039e97c5bd41002ff4f9f87143d5
LouisWT <liuwentong@web3.com>
2024-10-18 14:50:58 +0800

    feat: change sui signTransactionBlock version

--- src/sui_provider.js
+++ src/sui_provider.js
@@ -52,7 +52,7 @@ export class SuiProvider extends BaseProvider {
         on: this.on.bind(this),
       },
       "sui:signTransactionBlock": {
-        version: "2.0.0",
+        version: "1.0.0",
         signTransactionBlock: this.signTransactionBlock.bind(this),
       },
       "sui:signAndExecuteTransactionBlock": {
